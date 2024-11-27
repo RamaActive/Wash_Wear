dde4c8d733c91e2f685230ab18d68469f01086eb
RamaActive <Ramadanoladimeji101@gmail.com>
2024-11-27 11:37:14 +0000

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.wash_wear"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.wash_wear"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+
+    // ----- Importing scalable unit
+    implementation(libs.sdp.android)
+
+    // ------ Importing material design dependency
+    implementation (libs.material.v1110)
+    // ------ Importing country code picker
+    implementation(libs.ccp)
+
+    // ---- Importing Pin view
+    implementation (libs.pinview)
+
+
+    //ROUND IMAGE
+    implementation(libs.roundedimageview)
+
+    //ANIMATED IMAGES
+    implementation(libs.lottie)
+
+    //SIZES FOR DIFFERENT SCREENS
+    implementation(libs.ssp.android)
+    implementation(libs.sdp.android)
+
+    //NAVIGATION UI
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.picasso)
+    implementation(libs.glide)
+
+}
---
+++ app/src/main/java/com/example/wash_wear/Dashboard.java
@@ -0,0 +1,20 @@
+package com.example.wash_wear;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ---- making window fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // -------- Calling the Splash_Screen layout
+        setContentView(R.layout.activity_main);
+    }
+}
